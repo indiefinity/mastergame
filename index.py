488d3a2e188283171efd8ffd3d5d0568bbf9505f
viluzi <ville@kastell.tk>
2019-09-07 14:32:17 +0300

    23423

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import os
 import json
+import random as ran
 app = Flask(__name__, template_folder='./')
 @app.route("/")
 def index():
@@ -15,12 +16,20 @@ def create(id):
         f = open("./games/" + id + ".json", "w+")
         f.write('{"id":' + id + ',"playercount":1}')
         f.close()
+        f = open("./games/" + id + ".json","r")
+        data = f.read()
+        f.close()
+        jsonObj = json.loads(data)
+        jsonObj["started"] = False
+        f = open("./games/" + id + ".json","w")
+        f.seek(0)
+        f.write(json.dumps(jsonObj))
+        f.close()
         return "success"
 @app.route('/join/<id>', methods = ['POST'])
 def join(id):
     print(id)
     if os.path.exists('./games/' + id + ".json"):
-        boolean = 1
         f = open("./games/" + id + ".json","r")
         data = f.read()
         f.close()
@@ -31,9 +40,9 @@ def join(id):
         f.write(json.dumps(jsonObj))
         print(jsonObj)
         f.close()
+        return "exists"
     else:
-        boolean = 0
-    return str(boolean) 
+        return "error" 
 @app.route('/start/<id>', methods = ['POST'])
 def  start(id):
     f = open("./games/" + id + ".json","r")
@@ -46,5 +55,22 @@ def  start(id):
     f.write(json.dumps(jsonObj))
     f.close()
     return "started" + id
+@app.route('/ready/<id>', methods = ['POST'])
+def  ready(id):
+    f = open("./games/" + id + ".json","r")
+    data = f.read()
+    f.close()
+    jsonObj = json.loads(data)
+    if jsonObj["started"]:
+        return "started"
+    else:
+        return "not yet"
+@app.route('/client/<id>', methods = ['POST'])
+def client(id):
+    f = open("./games/" + id + ".json","r")
+    data = f.read()
+    f.close()
+    jsonObj = json.loads(data)
+    return jsonObj["q"]#'{"q":"If you were a car would you prefer to be","a":"White?","a2":"Black?","a3":"Gray?", "a4":"Yellow?"}'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
